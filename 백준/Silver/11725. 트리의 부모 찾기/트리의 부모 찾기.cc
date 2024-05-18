a8ed9badc43e6890f488dca1462f3cf88ba0c388
No-Jyun <165466867+No-Jyun@users.noreply.github.com>
2024-05-18 18:12:47 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 56 ms, Memory: 8584 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <string>
+#include <cstring>
+#include <cmath>	// abs for float, double
+#include <cstdlib> // abs for int, long int
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+
+	int n;
+	cin >> n;
+
+	vector<vector<int>> v(n + 1);
+	for (int i = 0; i < n - 1; i++) {
+		int a, b;
+		cin >> a >> b;
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+	int* chk = new int[n + 1];
+	fill(chk, chk + n + 1, 0);
+	int* parent = new int[n + 1];
+	fill(parent, parent + n + 1, 0);
+
+	queue<int> q;
+	q.push(1);
+	while (!q.empty()) {
+		int now = q.front();
+		q.pop();
+
+		if (!chk[now]) {
+			chk[now] = 1;
+
+			for (int i = 0; i < v[now].size(); i++) {
+				int tmp = v[now][i];
+				q.push(tmp);
+				if (!parent[tmp])parent[tmp] = now;
+			}
+		}
+	}
+
+	for (int i = 2; i <= n; i++) { cout << parent[i] << '\n'; }
+}
