b6d1b1e8c4800165fd3a6b0efc42299a0f8aa851
No-Jyun <165466867+No-Jyun@users.noreply.github.com>
2024-05-29 16:00:39 +0900

    [Gold V] Title: 동전 1, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <string>
+#include <cstring>
+#include <cmath>	// abs for float, double
+#include <cstdlib> // abs for int, long int
+using namespace std;
+
+int n, k;
+int coin[101];
+int dp[10001];
+int tmp[10001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> coin[i];
+		if (coin[i] > k) coin[i] = 0;
+	}
+
+	sort(coin, coin + n + 1);
+
+	for (int i = 1; i <= n; i++) {
+		if (coin[i]) {
+			fill(tmp, tmp + 10001, 0);
+
+			for (int j = 1; j <= k; j++) {
+				if (j + coin[i] <= k)dp[j + coin[i]] += dp[j];
+
+				if (j % coin[i] == 0)tmp[j]++;
+
+				dp[j] += tmp[j];
+			}
+		}
+	}
+
+	cout << dp[k];
+}
