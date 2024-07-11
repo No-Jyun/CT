538349225bd65a36e2af23c5addf40b0e283164f
No-Jyun <165466867+No-Jyun@users.noreply.github.com>
2024-07-11 11:47:57 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <string>
+#include <cstring>
+#include <cmath>	// abs for float, double
+#include <cstdlib> // abs for int, long int
+#define ULL unsigned long long
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << a + b - c << '\n';
+	cout << stoi(to_string(a) + to_string(b)) - stoi(to_string(c));
+}
