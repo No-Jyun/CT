f73fee341337103ca00ba55f0706025f0e3c44e4
No-Jyun <165466867+No-Jyun@users.noreply.github.com>
2024-07-13 18:00:36 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,22 +1,45 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <deque>
 #include <string>
+#include <cstring>
+#include <cmath>	// abs for float, double
+#include <cstdlib> // abs for int, long int
+#define ULL unsigned long long
 using namespace std;
 
+int r = 31;
+int M = 1234567891;
+ULL H;
+
+ULL Div(int c) {
+	if (c == 0 )return 1;
+	if (c == 1)return r;
+
+	ULL t = Div(c / 2);
+	if (c % 2)return (((t * t) % M) * r) % M;
+	else return (t * t) % M;
+}
+
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int l;
 	cin >> l;
 	string s;
 	cin >> s;
-	
-	long long hsh = 0;
-	for (int i = 0; i < s.length(); i++) {
-		long long tmp = s[i] - 96;
-		for (int k = 0; k < i; k++) {
-			tmp *= 31;
-		}
-		hsh += tmp;
+
+	for (int i = 0; i < l; i++) {
+		ULL n = s[i] - 'a' + 1;
+		H += (n * Div(i)) % M;
+		H %= M;
 	}
-	cout << hsh;
+	cout << H;
 }
