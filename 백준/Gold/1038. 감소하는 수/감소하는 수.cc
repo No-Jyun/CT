02905fe684682d43c4a64a6608ac0672d9c2de2a
No-Jyun <165466867+No-Jyun@users.noreply.github.com>
2024-08-13 13:14:59 +0900

    [Gold V] Title: 감소하는 수, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <string>
+#include <stack>
+#include <tuple>
+#include <math.h>
+using namespace std;
+
+int n;
+int index = 9;
+queue<string> q;
+
+void Go() {
+	while (1) {
+		queue<string> tmp;
+
+		while (!q.empty()) {
+			string nowN = q.front();
+			q.pop();
+			int lastN = nowN[nowN.size() - 1] - '0';
+
+			for (int i = 0; i <= 9 && lastN > i; i++) {
+				index++;
+				tmp.push(nowN + to_string(i));
+
+				if (index == n) {
+					cout << nowN + to_string(i);
+					return;
+				}
+			}
+		}
+		q = tmp;
+
+		if (tmp.empty()) {
+			cout << -1;
+			return;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+
+	if (n <= 9) { cout << n; }
+	else {
+		for (int i = 1; i <= 9; i++)q.push(to_string(i));
+
+		Go();
+	}
+}
