f0d0deb6160369060cf62c63e4729c94c02dfa1d
No-Jyun <165466867+No-Jyun@users.noreply.github.com>
2024-08-27 17:08:36 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <string>
+#include <cstring>
+#include <cmath>	// abs for float, double
+#include <cstdlib> // abs for int, long int
+#define ULL unsigned long long
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	cout << n << '\n' << 1;
+}
