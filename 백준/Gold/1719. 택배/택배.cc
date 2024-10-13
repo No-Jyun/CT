860adf8a59aa4fd40bbd91cbb12d15dd39bedca6
No-Jyun <165466867+No-Jyun@users.noreply.github.com>
2024-10-13 17:02:05 +0900

    [Gold III] Title: 택배, Time: 576 ms, Memory: 2604 KB -BaekjoonHub

---
+++ 백준/Gold/1719. 택배/택배.cc
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <string>
+#include <cstring>
+#include <cmath>	// abs for float, double
+#include <cstdlib> // abs for int, long int
+#define ULL unsigned long long
+#define INTMAX 2147483647
+using namespace std;
+
+int n, m;
+vector<pair<int, int>> edges[201];
+int way[201][201];
+vector<int> chk[201];
+
+void BFS(int stI) {
+	priority_queue<vector<int>> q;
+	q.push({ 0,stI });
+	chk[stI] = { 0,stI };
+
+	while (!q.empty()) {
+		vector<int> tmp = q.top();
+		q.pop();
+		int nowI = tmp[tmp.size() - 1];
+		int nowC = -tmp[0];
+
+		for (int i = 0; i < edges[nowI].size(); i++) {
+			vector<int> tmpV = tmp;
+			int tmpC = nowC + edges[nowI][i].second;
+			int tmpI = edges[nowI][i].first;
+
+			if (chk[tmpI][0] == -1) {
+				tmpV[0] = -tmpC;
+				tmpV.push_back(tmpI);
+				chk[tmpI] = tmpV;
+				q.push(tmpV);
+			}
+			else if (-chk[tmpI][0] > tmpC) {
+				tmpV[0] = -tmpC;
+				tmpV.push_back(tmpI);
+				chk[tmpI] = tmpV;
+				q.push(tmpV);
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (i != stI)way[stI][i] = chk[i][2];
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 1; i <= m; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		edges[a].push_back({ b,c });
+		edges[b].push_back({ a,c });
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int i = 1; i <= n; i++)chk[i] = { -1 };
+		BFS(i);
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (i == j)cout << '-' << ' ';
+			else cout << way[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+}
