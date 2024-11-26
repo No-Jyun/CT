6e9957866550a25bab69b8ed3f45fe059bc79dcb
No-Jyun <165466867+No-Jyun@users.noreply.github.com>
2024-11-26 23:03:24 +0900

    [Gold II] Title: 보석 도둑, Time: 172 ms, Memory: 20240 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -16,8 +16,8 @@ using namespace std;
 
 int n, k;
 ULL result;
-priority_queue<pair<int,int>> juw1;
-priority_queue<int> bag, juw2;
+map<int, int> bag;
+vector<pair<int, int>> v;
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -25,30 +25,25 @@ int main() {
 	cout.tie(NULL);
 
 	cin >> n >> k;
+	v.push_back({ -1,-1 });
 	for (int i = 1; i <= n; i++) {
 		int a, b;
 		cin >> a >> b;
-		juw1.push({ -a,-b });
+		v.push_back({ b,a });
 	}
-	
 	for (int i = 1; i <= k; i++) {
 		int a;
 		cin >> a;
-		bag.push(-a);
+		bag[a]++;
 	}
+	sort(v.begin(), v.end());
 
-	while (!bag.empty()) {
-		int topB = -bag.top();
-		bag.pop();
-
-		while (!juw1.empty() && -juw1.top().first <= topB) {
-			juw2.push(-juw1.top().second);
-			juw1.pop();
-		}
-
-		if (!juw2.empty()) {
-			result += juw2.top();
-			juw2.pop();
+	for (int i = n; i >= 1; i--) {
+		auto it = bag.lower_bound(v[i].second);
+		if (it != bag.end()) {
+			result += v[i].first;
+			if (it->second == 1)bag.erase(it);
+			else it->second--;
 		}
 	}
 
