f49a8f51457fcaffa3a8463f4827d5355eb7bbd8
No-Jyun <165466867+No-Jyun@users.noreply.github.com>
2024-12-22 14:31:54 +0900

    [Gold IV] Title: 전화번호 목록, Time: 524 ms, Memory: 3140 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <string>
+#include <cstring>
+#include <cmath>	// abs for float, double
+#include <cstdlib> // abs for int, long int
+#define ULL unsigned long long
+#define INTMAX 2147483647
+using namespace std;
+
+int n;
+string arr[10001];
+map<string, int> m;
+
+bool C() {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= arr[i].size(); j++) {
+			string t = arr[i].substr(0, j);
+			auto it = m.insert({ t,1 });
+
+			if (!it.second) return false;
+			if (j != arr[i].size())m.erase(t);
+		}
+	}
+	return true;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int t;
+	cin >> t;
+	while (t--) {
+		cin >> n;
+		for (int i = 1; i <= n; i++)cin >> arr[i];
+		sort(arr, arr + n + 1);
+
+		if (C())cout << "YES" << '\n';
+		else cout << "NO" << '\n';
+		m.clear();
+	}
+}
