f16431b79d40e0a8e7c95e7c44d32eb4121f8081
No-Jyun <165466867+No-Jyun@users.noreply.github.com>
2026-02-20 12:18:00 +0900

    [Gold III] Title: 비슷한 단어, Time: 52 ms, Memory: 11244 KB -BaekjoonHub

---
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int n;
+map<string, vector<int>> m;
+vector<string> origin;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		string str;
+		cin >> str;
+		origin.emplace_back(str);
+
+		for (int j = 1; j <= str.size(); j++)
+		{
+			//cout << str.substr(0, j) << ' ';
+			m[str.substr(0, j)].emplace_back(i);
+		}
+		//cout << "\n";
+	}
+
+	int maxS = 0;
+	int first = 0;
+	int second = 0;
+	for (auto it : m)
+	{
+		int curPreStrSize = it.first.size();
+		int curStrNum = it.second.size();
+
+		if (curStrNum <= 1)
+			continue;
+
+		if (maxS < curPreStrSize)
+		{
+			maxS = curPreStrSize;
+			first = it.second[0];
+			second = it.second[1];
+		}
+		else if (maxS == curPreStrSize)
+		{
+			int semiF = it.second[0];
+			int semiS = it.second[1];
+
+			if (semiF < first)
+			{
+				first = semiF;
+				second = semiS;
+			}
+		}
+	}
+	
+	cout << origin[first] << "\n" << origin[second];
+}
