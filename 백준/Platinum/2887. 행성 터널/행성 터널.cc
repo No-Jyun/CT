b1be782b61321650e6adeb20d9a83e6e5667c10c
No-Jyun <165466867+No-Jyun@users.noreply.github.com>
2026-04-14 15:48:45 +0900

    [Platinum V] Title: 행성 터널, Time: 100 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.cc
+++ 백준/Platinum/2887. 행성 터널/행성 터널.cc
@@ -5,18 +5,20 @@
 
 using namespace std;
 
-using LL = long long;
-using P = pair<LL, LL>;
+using P = pair<int, int>;
 
 int n;
+P posX[100000];
+P posY[100000];
+P posZ[100000];
 
 struct Onion
 {
 	int num = 0;
 	int parent = 0;
-};
+}onionArray[100000];
 
-LL GetParent(LL a, Onion* onionArr)
+int GetParent(int a, Onion* onionArr)
 {
 	if (onionArr[a].parent == onionArr[a].num)
 	{
@@ -26,6 +28,19 @@ LL GetParent(LL a, Onion* onionArr)
 	onionArr[a].parent = GetParent(onionArr[a].parent, onionArr);
 	return onionArr[a].parent;
 }
+// 같은 유니온에 포함된 친구들의 부모를 갱신함
+// ex
+// 1 -> 2 -> 3 -> 4 이런식으로 연결되어있고,
+// 1(num), 2(parent) / 2, 3 / 3, 4 라고 했을때 
+// GetParent(1, onionArr)을 호출하면
+// 1 != 2 이므로 onionArr[1].parent (현재 2) 자리에 GetParent(2, onionArr) 호출
+// 2 != 3 이므로 onionArr[2].parent (현재 3) 자리에 GetParent(3, onionArr) 호출
+// 3 != 4 이므로 onionArr[3].parent (현재 4) 자리에 GetParent(4, onionArr) 호출
+// 4 == 4 이므로 return onionArr[4].parent (즉, 4) 반환
+// onionArr[3].parent 에 4 대입 후 4 반환
+// onionArr[2].parent 에 4 대입 후 4 반환
+// onionArr[1].parent 에 4 대입 후 4 반환
+//
 
 int main()
 {
@@ -34,50 +49,48 @@ int main()
 	cout.tie(nullptr);
 
 	cin >> n;
-	vector<P> x(n);
-	vector<P> y(n);
-	vector<P> z(n);
-	Onion* onionArray = new Onion[n];
 
-	vector<pair<LL, P>> shortestEdges;
+	vector<pair<int, P>> shortestEdges;
 
 	for (int i = 0; i < n; i++)
 	{
-		cin >> x[i].first >> y[i].first >> z[i].first;
-		x[i].second = i; y[i].second = i; z[i].second = i;
+		cin >> posX[i].first >> posY[i].first >> posZ[i].first;
+		posX[i].second = i; posY[i].second = i; posZ[i].second = i;
 
 		onionArray[i].num = i;
 		onionArray[i].parent = i;
 	}
 	
-	sort(x.begin(), x.end());
-	sort(y.begin(), y.end());
-	sort(z.begin(), z.end());
+	std::sort(posX, posX + n);
+	std::sort(posY, posY + n);
+	std::sort(posZ, posZ + n);
 
 	for (int i = 1; i < n; i++)
 	{
-		LL length = abs(x[i].first - x[i - 1].first);
-		shortestEdges.emplace_back(pair<LL, P>(length, { x[i].second, x[i - 1].second }));
+		int length = abs(posX[i].first - posX[i - 1].first);
+		shortestEdges.emplace_back(pair<int, P>(length, { posX[i].second, posX[i - 1].second }));
 
-		length = abs(y[i].first - y[i - 1].first);
-		shortestEdges.emplace_back(pair<LL, P>(length, { y[i].second, y[i - 1].second }));
+		length = abs(posY[i].first - posY[i - 1].first);
+		shortestEdges.emplace_back(pair<int, P>(length, { posY[i].second, posY[i - 1].second }));
 
-		length = abs(z[i].first - z[i - 1].first);
-		shortestEdges.emplace_back(pair<LL, P>(length, { z[i].second, z[i - 1].second }));
+		length = abs(posZ[i].first - posZ[i - 1].first);
+		shortestEdges.emplace_back(pair<int, P>(length, { posZ[i].second, posZ[i - 1].second }));
 	}
 
-	sort(shortestEdges.begin(), shortestEdges.end());
-	LL answer = 0;
+	std::sort(shortestEdges.begin(), shortestEdges.end());
+	int answer = 0;
+	int maxEdge = n - 1;
+	int curEdge = 0;
 
 	for (int i = 0; i < shortestEdges.size(); i++)
 	{
-		LL curLen = shortestEdges[i].first;
-		LL a = shortestEdges[i].second.first;
-		LL b = shortestEdges[i].second.second;
+		int curLen = shortestEdges[i].first;
+		int a = shortestEdges[i].second.first;
+		int b = shortestEdges[i].second.second;
 
 		// 부모로 치환
-		LL parentA = GetParent(a, onionArray);
-		LL parentB = GetParent(b, onionArray);
+		int parentA = GetParent(a, onionArray);
+		int parentB = GetParent(b, onionArray);
 
 		// 두개의 유니온이 결합되어 있는지 확인
 		// 결합이 되어 있다면 패스
@@ -88,9 +101,12 @@ int main()
 		// 결합이 안되어 있다면 결합
 		else
 		{
+			curEdge++;
 			answer += curLen;
 			onionArray[parentA].parent = parentB;
-		}		
+		}
+
+		if (curEdge >= maxEdge) break;
 	}
 
 	cout << answer;
