ad0f117faef5400865183b2470f998916f162b96
No-Jyun <165466867+No-Jyun@users.noreply.github.com>
2026-04-16 19:06:51 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 352 ms, Memory: 21364 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -1,52 +1,81 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <stack>
-#include <map>
-#include <unordered_map>
-#include <queue>
-#include <deque>
-#include <string>
-#include <cstring>
-#include <cmath>	// abs for float, double
-#include <cstdlib> // abs for int, long int
-#define ULL unsigned long long
-#define INTMAX 2147483647
+#include <algorithm>
+
 using namespace std;
 
-int n, m, cost, maxC;
-vector<pair<int, int>> edges[100001];
-int chk[100001];
-priority_queue<pair<int, int>> q;
+struct Onion {
+	int num;
+	int parent;
+}onionArray[100000];
 
-int main() {
+int GetParent(int a)
+{
+	if (onionArray[a].num == onionArray[a].parent)
+	{
+		return onionArray[a].parent;
+	}
+
+	onionArray[a].parent = GetParent(onionArray[a].parent);
+	return onionArray[a].parent;
+}
+
+int main()
+{
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
+	int n, m;
 	cin >> n >> m;
-	while (m--) {
+
+	vector<pair<int, pair<int, int>>> edges;
+
+	for (int i = 0; i < n; i++)
+	{
+		onionArray[i].num = i;
+		onionArray[i].parent = i;
+	}
+
+	for (int i = 0; i < m; i++)
+	{
 		int a, b, c;
 		cin >> a >> b >> c;
-		edges[a].push_back({ b,c });
-		edges[b].push_back({ a,c });
+
+		edges.push_back({ c,{a - 1,b - 1} });
 	}
 
-	q.push({ 0,1 });
-	while (!q.empty()) {
-		int c = -q.top().first;
-		int a = q.top().second;
-		q.pop();
+	sort(edges.begin(), edges.end());
 
-		if (!chk[a]) {
-			chk[a] = 1;
-			cost += c;
-			maxC = max(maxC, c);
-			for (int i = 0; i < edges[a].size(); i++) {
-				if(!chk[edges[a][i].first])q.push({ -edges[a][i].second,edges[a][i].first });
-			}
+	int answer = 0;
+	int maxE = n - 1;
+	int countE = 0;
+	int maxCost = 0;
+	for (int i = 0; i < m; i++)
+	{
+		int curA = edges[i].second.first;
+		int curB = edges[i].second.second;
+		int curC = edges[i].first;
+
+		int parentA = GetParent(curA);
+		int parentB = GetParent(curB);
+
+		if (parentA == parentB)
+		{
+			continue;
 		}
+		else
+		{
+			onionArray[parentA].parent = parentB;
+			countE++;
+			answer += curC;
+
+			maxCost = maxCost > curC ? maxCost : curC;
+		}
+
+		if (countE >= maxE)
+			break;
 	}
 
-	cout << cost - maxC;
+	cout << answer - maxCost;
 }
